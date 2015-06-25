07db3a18ddded196f0da389a7b2b36009e693b13
Michael O. Church <michael.o.church@gmail.com>
2015-06-24 22:11:46 -0500

    Implement the S-expression parser

--- Lab2.hs
+++ Lab2.hs
@@ -15,3 +15,42 @@
 
 -- load a file that defines factorial and map and range. use at repl.
 -- none of this needs to be tail recursive or fast.
+
+module Main where
+
+import Control.Applicative hiding (many, (<|>))
+import Data.Char
+import Text.Parsec
+import Text.Parsec.Language (haskellDef)
+import Text.Parsec.Token (makeTokenParser, stringLiteral) 
+
+
+data SExp = SAtom String | SList [SExp] deriving (Eq, Show)
+
+parseAtom :: Parsec String u SExp
+parseAtom = SAtom <$> many (satisfy (\c -> c `notElem` "()\"" && (not . isSpace) c))
+
+-- I really don't feel like writing my own string literal parser. 
+stringLit :: Parsec String u String
+stringLit = stringLiteral $ makeTokenParser haskellDef
+
+enquote :: String -> String
+enquote s = "\"" ++ s ++ "\""
+
+parseString :: Parsec String u SExp
+parseString = (SAtom . enquote) <$> stringLit
+
+parseList :: Parsec String u SExp
+parseList = SList <$> (char '(' *> many space *> body <* many space <* char ')')
+  where body = sepBy parseSExp (many1 space)
+
+parseSExp :: Parsec String u SExp
+parseSExp = parseList <|> parseString <|> parseAtom
+
+data Lisp = LString String | LSymbol String | LNumber Double
+
+readLisp :: SExp -> Lisp
+readLisp = undefined
+
+main :: IO ()
+main = return ()
